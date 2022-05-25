94aef035c63a4d285c6f1fb634ac3c7972c74ff7
FedericoCapitani <federico.capitani@outlook.it>
2022-05-25 14:50:05 +0200

    add calc profit and get profit methods

--- index.php
+++ index.php
@@ -3,19 +3,40 @@ class Movie {
     public $name;
     public $length;
     public $release_year;
+    public $budget = 100;
+    public $earnings;
+    public $profit = 0;
 
-    function __construct($_name, $_length, $_release_year) {
+    function __construct($_name, $_length, $_release_year, $_earnings) {
         $this->name = $_name;
         $this->length = $_length;
         $this->release_year = $_release_year;
+        $this->earnings = $_earnings;
+    }
+
+    function calcProfit() {
+        $this->profit = $this->earnings - $this->budget;
+    }
+
+    function getProfit() {
+        return $this->profit;
     }
 }
 
-$alien = new Movie("alien","1:50h", 1980);
-$matrix = new Movie("matrix", "2.30h", 2001);
+$alien = new Movie("alien","1:50h", 1980, 150);
+$matrix = new Movie("matrix", "2.30h", 2001, 125);
+$alien->calcProfit();
+$matrix->calcProfit();
+
+$profit_alien = $alien->getProfit();
+$profit_matrix = $matrix->getProfit();
+
 
 var_dump($alien);
 var_dump($matrix);
+
+var_dump($profit_alien);
+var_dump($profit_matrix);
 ?>
 
 <!DOCTYPE html>
