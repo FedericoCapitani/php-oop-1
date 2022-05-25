0f86d324566d1ddf48b8d5fa206a770f8c3ca000
FedericoCapitani <federico.capitani@outlook.it>
2022-05-25 14:26:19 +0200

    add class movie

--- index.php
+++ index.php
@@ -1,5 +1,21 @@
 <?php
+class Movie {
+    public $name;
+    public $length;
+    public $release_year;
 
+    function __construct($_name, $_length, $_release_year) {
+        $this->name = $_name;
+        $this->length = $_length;
+        $this->release_year = $_release_year;
+    }
+}
+
+$alien = new Movie("alien","1:50h", 1980);
+$matrix = new Movie("matrix", "2.30h", 2001);
+
+var_dump($alien);
+var_dump($matrix);
 ?>
 
 <!DOCTYPE html>
